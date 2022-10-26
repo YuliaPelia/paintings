ec658a961a082e7373cdc6cc1d3921a8691aced7
Yulia <yuliapelia19@gmail.com>
2022-10-26 20:36:42 +0300

    modals

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,42 +1,55 @@
-// modal with animated
 const modals = () => {
 
-    let btnPressed = false;
+    // 5. Створюєм змінну яка буде відповідати за те що вона слідкує чи була нажата хоч одна кнопка
+    // по виклику модального вікна (для того щоб дізнатись чи була нажата хоч якась кнопка)
+    let btnPressed;
+    // ця змінна буде змінюватись коли користувач кліне на любу кнопку по виклику модального вікна
 
+    // trigger - селектор нашої кнопки по якій ми будемо клікати
+    // modal - модальне вікно яке ми будемо відкривати
+    // close - селекор який закриває модальне вікно (хрестик)
+    function bindModal(triggerSelector, modalSelector, closeSelector, gift = false) {
 
-    function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
-        
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
               close = document.querySelector(closeSelector),  
               windows = document.querySelectorAll('[data-modal]'),
               scroll = calcScroll();
 
+    
+
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
+                
+                // e.target - чи точно існує той елемент на який клікнув користувач
                 if(e.target) {
-                    e.preventDefault();
+                    e.preventDefault(); // відміняєм стандартну поведінку браузера
                 }
 
-                btnPressed = true;
-
-                if(destroy) {
-                    item.remove();
-                }
-
+                btnPressed = true; // користувач клікнув на кнопку (trigger)
+                
+                // берем всі модальні вікна які є і при відкритті нового модального вікна старі всі закриваються
                 windows.forEach(item => {
                     item.style.display = 'none';
-                    // animated - для того щоб правильно запрацювали анімації
-                    // fadeIn - для того щоб красиво появлялось модальне вікно
                     item.classList.add('animated', 'fadeIn');
                 });
 
+                // коли користувач нажимає на подарунок він пропадає зі сторінки
+                if (gift) {
+                    item.remove();
+                }
+
                 modal.style.display = "block";
+                // блокуємо scroll сторінки при відкритті модального вікна
                 document.body.style.overflow = "hidden";
+                // document.body.classList.add('modal-open');
                 document.body.style.marginRight = `${scroll}px`;
+                document.querySelector('.fixed-gift').style.marginRight = `${scroll}px`;
+
+    
             });
         });
-
+        // закриття модального вікна при натисканні на хрестик
         close.addEventListener('click', () => {
             windows.forEach(item => {
                 item.style.display = 'none';
@@ -44,9 +57,14 @@ const modals = () => {
 
             modal.style.display = "none";
             document.body.style.overflow = "";
+            // document.body.classList.remove('modal-open');
             document.body.style.marginRight = `0px`;
+            document.querySelector('.fixed-gift').style.marginRight = `0px`;
+
 
         });
+
+        // закриття модального вікна при натисканні на подложку
         modal.addEventListener('click', (e) => {
             if(e.target === modal) {
                 windows.forEach(item => {
@@ -54,29 +72,38 @@ const modals = () => {
                 });
                 modal.style.display = "none";
                 document.body.style.overflow = "";
+                // document.body.classList.remove('modal-open'); 
                 document.body.style.marginRight = `0px`;
+                document.querySelector('.fixed-gift').style.marginRight = `0px`;
 
             }
         });
     }
 
+    
+    
+    // функція відкриття модального вікна через певний проміжок часу
+    // selector - модальне вікно
+    // time - час через який відкриється це модальне вікно
     function showModalByTime(selector, time) {
         setTimeout(function() {
-            let display;
+            // якщо користувач вже відкрив модальне вікно тоді ця функція відміняється
+            let d;
 
             document.querySelectorAll('[data-modal]').forEach(item => {
                 if(getComputedStyle(item).display !== 'none') {
-                    display = "block";
+                    d = 'block';
                 }
             });
 
-            if(!display) {
+            // якщо користувач не відкрив ні одного модального вікна, тоді ми його показуємо через певний проміжок часу
+            if(!d) {
                 document.querySelector(selector).style.display = "block";
                 document.body.style.overflow = "hidden";
                 let scroll = calcScroll();
                 document.body.style.marginRight = `${scroll}px`;
+                document.querySelector('.fixed-gift').style.marginRight = `${scroll}px`;
             }
-
         }, time);
     }
 
@@ -97,13 +124,10 @@ const modals = () => {
 
     function openByScroll(selector) {
         window.addEventListener('scroll', () => {
-            // для підтримування старших браузерів
-            let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-            // чи догортав користувач сторінку до кінця
-            if(!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= 
+            if(!btnPressed && (window.scrollY + document.documentElement.clientHeight >= 
                 document.documentElement.scrollHeight)) {
                     document.querySelector(selector).click();
-            }
+                }
         });
     }
 
@@ -111,7 +135,7 @@ const modals = () => {
     bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
     bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
     openByScroll('.fixed-gift');
-    // showModalByTime('.popup-consultation', 5000); 
+    // showModalByTime('.popup-consultation', 60000);
 };
 
 export default modals;
